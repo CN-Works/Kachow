b44b5c51fb043c390c94e4342584390107b9831f
ChernyyOrel <chernyyorel.all@gmail.com>
2023-06-28 16:40:24 +0200

    Chore : Author integration

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -1,31 +1,33 @@
 <?php
 
 $topics = $result["data"]['topics'];
+use App\DAO;
     
 ?>
+<div class="topiclist-main">
+    <h1>liste topics</h1>
 
-<h1>liste topics</h1>
-
-<?php
-foreach($topics as $topic ){
-
-    ?>
-
-    <div class="topiclist-card">
-        <img class="topiclist-card-image unselectable" src="<?=$topic->getBanner()?>" alt="">
-        <h2 class="topiclist-card-title"><?=$topic->getTitle()?></h2>
-        <p class='topiclist-card-description'>Auteur <?= $topic->getId() ?></p>
-        <?=
-            // Vérifie si la description existe et n'est pas nulle
-            $description = $topic->getDescription();
-            if (isset($description)) {
-                return "<p class='topiclist-card-description'>".$description."</p>";
-            }
+    <?php
+    foreach($topics as $topic ){
 
         ?>
-    </div>
+        <div class="topiclist-card">
+            <img class="topiclist-card-image unselectable" src="<?=$topic->getBanner()?>" alt="">
+            <h2 class="topiclist-card-title"><?=$topic->getTitle()?></h2>
+            <p class='topiclist-card-description'>Ouvert par <?= $topic->getTopicAuthor()[0]["username"] ?></p>
+            <?=
+                // Vérifie si la description existe et n'est pas nulle
+                $description = $topic->getDescription();
+                if (isset($description)) {
+                    return "<p class='topiclist-card-description'>".$description."</p>";
+                }
+
+            ?>
+        </div>
     <?php
-}
+    } ?>
+</div>
+
 
 
   
