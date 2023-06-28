9a64297952a68e62b67d07b3986250e988c93a3e
ChernyyOrel <chernyyorel.all@gmail.com>
2023-06-28 15:00:48 +0200

    Chore : replacing main container (page)

--- view/layout.php
+++ view/layout.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +7,7 @@
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
-    <title>FORUM</title>
+    <title>PassionEssence.com</title>
 </head>
 <body>
     <div id="wrapper"> 
@@ -52,9 +52,7 @@
                 </nav>
             </header>
             
-            <main id="forum">
-                <?= $page ?>
-            </main>
+            <?= $page ?>
         </div>
         <footer>
             <p>&copy; 2020 - Forum CDA - <a href="/home/forumRules.html">Règlement du forum</a> - <a href="">Mentions légales</a></p>
