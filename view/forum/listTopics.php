6fb8ff80b9a50495dd5bc31f20a404268b478e87
ChernyyOrel <chernyyorel.all@gmail.com>
2023-06-28 15:01:18 +0200

    Fix : removing css from html file

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -10,55 +10,18 @@ $topics = $result["data"]['topics'];
 foreach($topics as $topic ){
 
     ?>
-    <style>
-        * {
-            font-family: "Poppins", sans-serif;
-            padding : 0;
-            margin : 0;
-            box-sizing: border-box;
-        }
-
-        .topiclist-card {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 5px;
-
-            background-color: #26292F;
-            border-radius: 10px;
-
-            max-width: 35%;
-            min-height: 400px;
-            margin: 10px;
-
-            color: #ffffff;
-
-            transition: all, 0.5s;
-        }
-
-        .topiclist-card:hover {
-            background-color: #5E747F;
-        }
-
-        .topiclist-card-image {
-            max-height: 35%;
-        }
-
-        .topiclist-card-description {
-            font-style: italic;
-        }
-    </style>
 
     <div class="topiclist-card">
-        <img class="topiclist-card-image" src="<?=$topic->getBanner()?>" alt="" style=" width: 100%;">
+        <img class="topiclist-card-image" src="<?=$topic->getBanner()?>" alt="">
         <h2 class="topiclist-card-title"><?=$topic->getTitle()?></h2>
+        <p class='topiclist-card-description'>Auteur <?= $topic->getId() ?></p>
         <?=
-            // Vérifie si la description existe (not NULL or "none")
+            // Vérifie si la description existe et n'est pas nulle
             $description = $topic->getDescription();
             if (isset($description)) {
                 return "<p class='topiclist-card-description'>".$description."</p>";
             }
+
         ?>
     </div>
     <?php
