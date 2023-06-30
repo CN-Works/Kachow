372c785d750882a210bb54cf19cc4a67f0044b83
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-06-30 09:06:35 +0200

    Changing some texts :

--- view/forum/listTopics.php
+++ view/forum/listTopics.php
@@ -7,25 +7,19 @@ use App\DAO;
 <div class="topiclist-main">
     <h1>liste topics</h1>
 
-    <?php
-    foreach($topics as $topic ){
-
-        ?>
-        <div class="topiclist-card">
-            <img class="topiclist-card-image unselectable" src="<?=$topic->getBanner()?>" alt="">
-            <h2 class="topiclist-card-title"><?=$topic->getTitle()?></h2>
-            <p class='topiclist-card-description'>Ouvert par <?= $topic->getTopicAuthor()["username"] ?></p>
-            <?=
-                // Vérifie si la description existe et n'est pas nulle
-                $description = $topic->getDescription();
-                if (isset($description)) {
-                    return "<p class='topiclist-card-description'>".$description."</p>";
-                }
+    <div class="topiclist-allcards">
+        <?php
+        foreach($topics as $topic ){
 
             ?>
-        </div>
-    <?php
-    } ?>
+            <div class="topiclist-card">
+                <img class="topiclist-card-image unselectable" src="<?=$topic->getBanner()?>" alt="">
+                <h2 class="topiclist-card-title"><?=$topic->getTitle()?></h2>
+                <p class='topiclist-card-description'>Ouvert par <?= $topic->getTopicAuthor()["username"] ?> le <?= $topic->getCreationdate()?></p>
+            </div>
+        <?php
+        } ?>
+    </div>
 </div>
 
 
