e589292fbc9a29f819b4f0d5d8915a41c05d0d66
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-02 20:27:04 +0200

    Feat : Home page text

--- view/home.php
+++ view/home.php
@@ -1,7 +1,11 @@
 <div class="home-main">
     <div class="home-pageone">
-        <video autoplay muted loop class="home-background">
-            <source class="home-background-video" src="./public/videos/SportsLosAngeles.mp4" type="video/mp4">
+        <div class="home-pageone-textzone unselectable">
+            <h2 class="home-pageone-title">Vrouum vrouum..</h2>
+            <p class="home-pageone-subtitle">Une passion, une odeur, un forum.</p>
+        </div>
+        <video autoplay muted loop class="home-pageone-background">
+            <source class="home-pageonebackground-video" src="./public/videos/SportsLosAngeles.mp4" type="video/mp4">
         </video>
     </div>
 </div>
