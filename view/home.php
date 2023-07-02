17222835c1b3a2949ae1a2f9541908f2e07e0dc8
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-02 19:25:04 +0200

    Chore : background video

--- view/home.php
+++ view/home.php
@@ -1,9 +1,7 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
-
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
-
-<p>
-    <a href="/security/login.html">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="/security/register.html">S'inscrire</a>
-</p>
+<div class="home-main">
+    <div class="home-pageone">
+        <video autoplay muted loop class="home-background">
+            <source class="home-background-video" src="./public/videos/SportsLosAngeles.mp4" type="video/mp4">
+        </video>
+    </div>
+</div>
