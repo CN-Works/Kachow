03a758bd4465535f12a903492da364bf3d7a898e
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-03 22:36:02 +0200

    Chore : adding card text

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -2,6 +2,8 @@
 
 $categories = $result["data"]['categories'];
 use App\DAO;
+
+$card_count = 1;
     
 ?>
 
@@ -13,14 +15,17 @@ use App\DAO;
         foreach($categories as $category) {
             ?>
             <div class="allcategory-card">
-                    <?php
-                        $banner = $category->getImage();
+                <?php
+                    $banner = $category->getImage();
 
-                        if (isset($banner)) {
-                            echo "<figure class=''><img src='$banner' alt='Bannière de la catégorie ".$category->getLabel()."'></figure>";
-                        }
-                    ?>
-                <h2><?= $category->getLabel() ?></h2>
+                    if (isset($banner)) {
+                        echo "<figure class='allcategory-card-bannerfig'><img class='allcategory-card-banner' src='$banner' alt='Bannière de la catégorie ".$category->getLabel()."'></figure>";
+                    }
+                ?>
+                <div class="allcategory-card-textside">
+                    <h2 class="allcategory-card-title"><?= $category->getLabel() ?></h2>
+                    <p class="allcategory-card-subtitle"><?= $category->getDescription() ?></p>
+                </div>
             </div>
             <?php
         }
