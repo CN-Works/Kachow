876b31c849e6075ecda8b75ef119e8d4331079f9
ChernyyOrel <chernyyorel.all@gmail.com>
2023-07-06 16:07:38 +0200

    Fix : unselectable images

--- view/forum/topicDetails.php
+++ view/forum/topicDetails.php
@@ -14,7 +14,7 @@ use App\DAO;
         <div class="topicdetails-topicinfos-left">
             <div class="topicdetails-topicinfos-authorpart">
                 <figure class="topicdetails-topicinfos-authorfig">
-                    <img class="topicdetails-topicinfos-authorimage" src="<?= $topic->getUser()->getProfileimage()?>" alt="Photo de profile de <?= $topic->getUser()->getUsername()?>">
+                    <img class="topicdetails-topicinfos-authorimage unselectable" src="<?= $topic->getUser()->getProfileimage()?>" alt="Photo de profile de <?= $topic->getUser()->getUsername()?>">
                 </figure>
                 <h3 class="topicdetails-topicinfos-author-username"><?= $topic->getUser()->getUsername()?></h3>
             </div>
@@ -23,7 +23,7 @@ use App\DAO;
         </div>
 
         <figure class="topicdetails-topicinfos-bannerfig" >
-            <img class="topicdetails-topicinfos-banner" src="<?= $topic->getBanner()?>" alt="Bannière du topic '<?= $topic->getTitle()?>' de <?= $topic->getUser()->getUsername()?>">
+            <img class="topicdetails-topicinfos-banner unselectable" src="<?= $topic->getBanner()?>" alt="Bannière du topic '<?= $topic->getTitle()?>' de <?= $topic->getUser()->getUsername()?>">
         </figure>
     </div>
 
