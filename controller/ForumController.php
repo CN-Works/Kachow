a8a50e33c38822d642dcb4d2876b63f519ce5254
ChernyyOrel <chernyyorel.all@gmail.com>
2023-07-06 11:23:40 +0200

    Chore : Topic details method

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -25,6 +25,22 @@
         
         }
 
+        public function TopicDetails($topicId){
+            // J'utilise le &er paramère en Id comme expliqué dans l'index, on doit donc utiliser le filter_var
+            $topicId = filter_var($topicId,FILTER_VALIDATE_INT);
+
+
+            $topicManager = new TopicManager();
+ 
+            return [
+                 "view" => VIEW_DIR."forum/topicDetails.php",
+                 "data" => [
+                     "topicDetails" => $topicManager->findOneById($topicId)
+                 ]
+             ];
+         
+         }
+
         public function AllCategories() {
 
             $categoryManager = new CategoryManager;
