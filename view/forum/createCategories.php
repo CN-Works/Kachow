9ea12fe5513907517b668af0ab00b88651afa7af
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-07 09:48:53 +0200

    Chore : Create "Adding" form

---
+++ view/forum/createCategories.php
@@ -0,0 +1,21 @@
+<?php
+
+$categories = $result["data"]['allcategories'];
+use App\DAO;
+    
+?>
+
+<h2>Créer une catégorie</h2>
+
+<form action="index.php?ctrl=forum&action=CreateCategory" method="post">
+    <p>Nom de catégorie</p>
+    <input type="text" name="category-name">
+
+    <p>Image</p>
+    <input type="text" name="category-image">
+
+    <p>Description</p>
+    <input type="text" name="category-description">
+
+    <input type="submit" value="Créer">
+</form>
