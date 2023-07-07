eb87d6e9147d60ebef3c2118d00ed669eff8a06b
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-07 14:18:52 +0200

    Create listRedactions.php

---
+++ view/forum/listRedactions.php
@@ -0,0 +1,7 @@
+<div class="">
+
+    <a href="index.php?ctrl=forum&action=CreateCategoryForm">Créer une catégorie</a>
+
+
+    <a href="index.php?ctrl=forum&action=CreateTopicForm">Créer un topic</a>
+</div>
