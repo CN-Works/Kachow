3358076b1f90f8883cce3f9b95156aca6fa6354e
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-09 22:14:04 +0200

    Chore : Category form creation (default)

--- view/forum/createCategories.php
+++ view/forum/createCategories.php
@@ -4,17 +4,24 @@ use App\DAO;
     
 ?>
 
-<h2>Créer une catégorie</h2>
+<div class="creation-form-main">
+    <figure class="creation-form-imagefig">
+        <img class="creation-form-image" src="https://www.hdcarwallpapers.com/walls/ferrari_499p_le_mans_hypercar_2022_4k-HD.jpg" alt="A cool Ferrari image">
+    </figure>
+    <div class="creation-form-right">
+        <form class="creation-form" action="index.php?ctrl=forum&action=CreateCategory" method="post">
+            <h2 class="creation-form-title">Créer une catégorie</h2>
 
-<form action="index.php?ctrl=forum&action=CreateCategory" method="post">
-    <p>Nom de catégorie</p>
-    <input type="text" name="category-name">
+            <p>Nom de catégorie</p>
+            <input type="text" name="category-name">
 
-    <p>Image</p>
-    <input type="text" name="category-image">
+            <p>Image</p>
+            <input type="text" name="category-image">
 
-    <p>Description</p>
-    <input type="text" name="category-description">
+            <p>Description</p>
+            <input type="text" name="category-description">
 
-    <input type="submit" value="Créer">
-</form>
+            <input type="submit" value="Créer">
+        </form>
+    </div>
+</div>
