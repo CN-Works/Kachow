2ad8ae08d3a4786f23eedd2d936b8c350e353197
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-10 21:35:22 +0200

    Chore : Post's entity getters & setters

--- model/entities/Post.php
+++ model/entities/Post.php
@@ -15,6 +15,7 @@
         public function __construct($data){         
             $this->hydrate($data);        
         }
+        
 
         /**
          * Get the value of id
@@ -22,6 +23,26 @@
         public function getId()
         {
                 return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of content
+         */ 
+        public function getContent()
+        {
+                return $this->content;
         }
 
         /**
@@ -37,6 +58,26 @@
         }
 
         /**
+         * Get the value of creationdate
+         */ 
+        public function getCreationdate()
+        {
+                return $this->creationdate;
+        }
+
+        /**
+         * Set the value of creationdate
+         *
+         * @return  self
+         */ 
+        public function setCreationdate($creationdate)
+        {
+                $this->creationdate = $creationdate;
+
+                return $this;
+        }
+
+        /**
          * Get the value of user
          */ 
         public function getUser()
@@ -45,10 +86,34 @@
         }
 
         /**
+         * Set the value of user
+         *
+         * @return  self
+         */ 
+        public function setUser($user)
+        {
+                $this->user = $user;
+
+                return $this;
+        }
+
+        /**
          * Get the value of topic
          */ 
         public function getTopic()
         {
                 return $this->topic;
+        }
+
+        /**
+         * Set the value of topic
+         *
+         * @return  self
+         */ 
+        public function setTopic($topic)
+        {
+                $this->topic = $topic;
+
+                return $this;
         }
     }
