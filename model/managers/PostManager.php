8bb342b0c6ba15ef585439c9286bd110b0a07710
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-10 21:35:41 +0200

    Chore : Listing all posts by topic id

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -15,5 +15,19 @@
             parent::connect();
         }
 
+        // Un select classique mais avec un argument "id" pour l'id du topic
+        public function findAllPostsByTopicId($id,$table, $order = null){
+            $orderQuery = ($order) ? "ORDER BY ".$order[0]. " ".$order[1] : "";
+
+            $sql = "SELECT *
+                    FROM ".$this->tableName." t
+                    WHERE t.".$table." = :id
+                    ".$orderQuery;
+
+            return $this->getMultipleResults(
+                DAO::select($sql, ['id' => $id]),
+                $this->className
+            );
+        }
 
     }
