fd79b27871d020b59a3812f681a8c50411cf97d5
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-12 00:42:09 +0200

    Fix : topic list where there's no topic

--- view/forum/listTopicsByCategory.php
+++ view/forum/listTopicsByCategory.php
@@ -13,6 +13,9 @@ use App\DAO;
             Tout les derniers articles rédigés par récemment, écrit par les membres de la communauté.
             <br>
             <?= $categoryInfo->getDescription() ?>
+            <br>
+            <br>
+            <a class="general-toppage-redactionlink unselectable" href="index.php?ctrl=forum&action=CreateCategoryForm">Rédigez le vôtre !</a>
             <!-- <br>
             <br>
             <a class="general-toppage-redactionlink" href="index.php?ctrl=forum&action=CreateCategoryForm">Rédigez le vôtre !</a> -->
@@ -21,35 +24,40 @@ use App\DAO;
 
     <div class="topiclist-allcards">
         <?php
-        foreach($topicsfromcategory as $topic ){
 
-            ?>
-            <div class="topiclist-card">
-                <?php
-                $banner = $topic->getBanner();
-
-                if (isset($banner)) {
-                    echo "<figure class='topiclist-card-figure'>
-                    <img class='topiclist-card-image unselectable' src='$banner' alt=''>
-                    </figure>";
-                }
+        if (isset($topicsfromcategory)) {
+            foreach($topicsfromcategory as $topic ){
 
                 ?>
-
-                <h2 class="topiclist-card-title"><?=$topic->getTitle()?></h2>
-                <div class="topiclist-card-description">
-                    <div class="topiclist-card-description-author">
-                        <p class="topiclist-card-description-authortext"><?= $topic->getUser()->getUsername() ?></p>
+                <div class="topiclist-card">
+                    <?php
+                    $banner = $topic->getBanner();
+    
+                    if (isset($banner)) {
+                        echo "<figure class='topiclist-card-figure'>
+                        <img class='topiclist-card-image unselectable' src='$banner' alt=''>
+                        </figure>";
+                    }
+    
+                    ?>
+    
+                    <h2 class="topiclist-card-title"><?=$topic->getTitle()?></h2>
+                    <div class="topiclist-card-description">
+                        <div class="topiclist-card-description-author">
+                            <p class="topiclist-card-description-authortext"><?= $topic->getUser()->getUsername() ?></p>
+                        </div>
+                        <p>le <?= $topic->getCreationdate()?></p>
                     </div>
-                    <p>le <?= $topic->getCreationdate()?></p>
+    
+                    <a class="topiclist-card-detail" href="index.php?ctrl=forum&action=topicDetails&id=<?=$topic->getId()?>">
+                        <p class="topiclist-card-detail-text unselectable">Lire</p>
+                    </a>
                 </div>
+            <?php
+            }
+        }
 
-                <a class="topiclist-card-detail" href="index.php?ctrl=forum&action=topicDetails&id=<?=$topic->getId()?>">
-                    <p class="topiclist-card-detail-text unselectable">Lire</p>
-                </a>
-            </div>
-        <?php
-        } ?>
+        ?>
     </div>
 </div>
 
