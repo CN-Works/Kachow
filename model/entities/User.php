153b9592a56c229898472a8af8e564090d35c2c0
ChernyyOrel <chernyyorel.all@gmail.com>
2023-07-13 14:42:06 +0200

    Chore : adding setters to User.php

--- model/entities/User.php
+++ model/entities/User.php
@@ -2,6 +2,7 @@
     namespace Model\Entities;
 
     use App\Entity;
+    use App\DAO;
 
     final class User extends Entity{
 
@@ -16,11 +17,17 @@
         public function __construct($data){         
             $this->hydrate($data);        
         }
- 
 
         public function getId()
         {
                 return $this->id;
+        }
+
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
         }
 
 
@@ -29,31 +36,70 @@
                 return $this->username;
         }
 
+        public function setUsername($username)
+        {
+                $this->username = $username;
+
+                return $this;
+        }
 
         public function getDescription()
         {
                 return $this->description;
         }
 
-        public function getCreationdate(){
-            $formattedDate = $this->creationdate->format("d/m/Y, H:i:s");
-            return $formattedDate;
+        public function setDescription($description)
+        {
+                $this->description = $description;
+
+                return $this;
         }
 
+        public function getPassword()
+        {
+                return $this->password;
+        }
+
+        public function setPassword($password)
+        {
+                $this->password = $password;
+
+                return $this;
+        }
+
+        public function getCreationdate(){
+                $formattedDate = $this->creationdate->format("d/m/Y, H:i:s");
+                return $formattedDate;
+        }
+
+        public function setCreationdate($creationdate)
+        {
+                $this->creationdate = $creationdate;
+
+                return $this;
+        }
 
         public function getRole()
         {
                 return $this->role;
         }
+ 
+        public function setRole($role)
+        {
+                $this->role = $role;
 
+                return $this;
+        }
 
         public function getProfileimage()
         {
                 return $this->profileimage;
         }
 
-        public function getPassword()
+        public function setProfileimage($profileimage)
         {
-                return $this->password;
+                $this->profileimage = $profileimage;
+
+                return $this;
         }
     }
