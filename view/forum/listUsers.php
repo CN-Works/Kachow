1db61909745ca43c522bbafce84c6c3a6edc44eb
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-15 23:46:20 +0200

    Feat : basic users list page

---
+++ view/forum/listUsers.php
@@ -0,0 +1,29 @@
+<?php
+
+$allUsers = $result["data"]["AllUsers"];
+use App\DAO;
+
+?>
+
+<div class="listUsers-main">
+    <div class="listUsers-head">
+        <h2>Gestion des utilisateurs</h2>
+        <p>Une liste de tout les utilisateurs du forum PassionEssence.</p>
+    </div>
+
+    <div class="listUsers-cards">
+        <?php
+            foreach($allUsers as $user) {
+                ?>
+                <div class="listUsers-card">
+                    <figure class="listUsers-card-profilefig">
+                        <img class="listUsers-card-profileimage" src="<?= $user->getProfileimage() ?>" alt="Profile picture of <?= $user->getUsername() ?>">
+                    </figure>
+                    <h3><?= $user->getUsername() ?></h3>
+                    <p><?= $user->getDescription() ?></p>
+                </div>
+                <?php
+            }
+        ?>
+    </div>
+</div>
